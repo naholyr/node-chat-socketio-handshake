ad3336f15281063923f714ec45c6d913bc931a82
Nicolas Chambrier <naholyr@gmail.com>
2011-07-28 00:00:47 +0200

    Added websocket authentication based on user's session

--- app.js
+++ app.js
@@ -12,12 +12,13 @@ app.configure(function() {
   // Allow parsing cookies from request headers
   this.use(express.cookieParser());
   // Session management
+  // Internal session data storage engine, this is the default engine embedded with connect.
+  // Much more can be found as external modules (Redis, Mongo, Mysql, file...). look at "npm search connect session store"
+  this.sessionStore = new express.session.MemoryStore({ reapInterval: 60000 * 10 });
   this.use(express.session({
     // Private crypting key
     "secret": "some private string",
-    // Internal session data storage engine, this is the default engine embedded with connect.
-    // Much more can be found as external modules (Redis, Mongo, Mysql, file...). look at "npm search connect session store"
-    "store":  new express.session.MemoryStore({ reapInterval: 60000 * 10 })
+    "store":  this.sessionStore
   }));
   // Allow parsing form data
   this.use(express.bodyParser());
@@ -98,26 +99,42 @@ app.post("/login", function (req, res) {
 
 /** WebSocket */
 var sockets = require('socket.io').listen(app).of('/chat');
+const parseCookie = require('connect').utils.parseCookie;
+sockets.authorization(function (handshakeData, callback) {
+  // Read cookies from handshake headers
+  var cookies = parseCookie(handshakeData.headers.cookie);
+  // We're now able to retrieve session ID
+  var sessionID = cookies['connect.sid'];
+  // No session? Refuse connection
+  if (!sessionID) {
+    callback('No session', false);
+  } else {
+    // Store session ID in handshake data, we'll use it later to associate
+    // session with open sockets
+    handshakeData.sessionID = sessionID;
+    // On récupère la session utilisateur, et on en extrait son username
+    app.sessionStore.get(sessionID, function (err, session) {
+      if (!err && session && session.username) {
+        // On stocke ce username dans les données de l'authentification, pour réutilisation directe plus tard
+        handshakeData.username = session.username;
+        // OK, on accepte la connexion
+        callback(null, true);
+      } else {
+        // Session incomplète, ou non trouvée
+        callback(err || 'User not authenticated', false);
+      }
+    });
+  }
+});
 sockets.on('connection', function (socket) { // New client
-  var socket_username = null;
-  // User sends his username
-  socket.on('user', function (username) {
-    socket_username = username;
-    sockets.emit('join', username, Date.now());
-  });
+  sockets.emit('join', socket.handshake.username, Date.now());
   // When user leaves
   socket.on('disconnect', function () {
-    if (socket_username) {
-      sockets.emit('bye', socket_username, Date.now());
-    }
+    sockets.emit('bye', socket.handshake.username, Date.now());
   });
   // New message from client = "write" event
   socket.on('write', function (message) {
-    if (socket_username) {
-      sockets.emit('message', socket_username, message, Date.now());
-    } else {
-      socket.emit('error', 'Username is not set yet');
-    }
+    sockets.emit('message', socket.handshake.username, message, Date.now());
   });
 });
 
