384ac56466a09122614f7b6ae1516b2554f244d9
Nicolas Chambrier <naholyr@gmail.com>
2011-07-27 23:55:35 +0200

    Added websocket basic chat

--- app.js
+++ app.js
@@ -96,6 +96,31 @@ app.post("/login", function (req, res) {
   }
 });
 
+/** WebSocket */
+var sockets = require('socket.io').listen(app).of('/chat');
+sockets.on('connection', function (socket) { // New client
+  var socket_username = null;
+  // User sends his username
+  socket.on('user', function (username) {
+    socket_username = username;
+    sockets.emit('join', username, Date.now());
+  });
+  // When user leaves
+  socket.on('disconnect', function () {
+    if (socket_username) {
+      sockets.emit('bye', socket_username, Date.now());
+    }
+  });
+  // New message from client = "write" event
+  socket.on('write', function (message) {
+    if (socket_username) {
+      sockets.emit('message', socket_username, message, Date.now());
+    } else {
+      socket.emit('error', 'Username is not set yet');
+    }
+  });
+});
+
 /** Start server */
 if (!module.parent) {
   app.listen(port)
