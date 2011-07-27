304b751be26ba1603815c17df53f4a6af93402ff
Nicolas Chambrier <naholyr@gmail.com>
2011-07-27 23:38:17 +0200

    Route "/session-index"

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ app.configure(function() {
   this.set('views', path.join(__dirname, 'views'));
   this.set('view engine', 'ejs');
   this.use(express.static(path.join(__dirname, '/public')));
+  // Allow parsing cookies from request headers
+  this.use(express.cookieParser());
+  // Session management
+  this.use(express.session({
+    // Private crypting key
+    "secret": "some private string",
+    // Internal session data storage engine, this is the default engine embedded with connect.
+    // Much more can be found as external modules (Redis, Mongo, Mysql, file...). look at "npm search connect session store"
+    "store":  new express.session.MemoryStore({ reapInterval: 60000 * 10 })
+  }));
 });
 app.configure('development', function(){
   this.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -21,7 +31,16 @@ app.configure('production', function(){
 app.get('/', function (req, res, next) {
   res.render('index');
 });
- 
+app.get('/session-index', function (req, res, next) {
+  // Increment "index" in session
+  req.session.index = (req.session.index || 0) + 1;
+  // View "session-index.ejs"
+  res.render('session-index', {
+    "index":  req.session.index,
+    "sessId": req.sessionID
+  });
+});
+
 /** Start server */
 if (!module.parent) {
   app.listen(port)
